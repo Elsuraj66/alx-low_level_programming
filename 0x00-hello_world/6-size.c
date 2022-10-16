9ae964f12e17619dc89b2c8a79c258c3de8c4977
elsuraj66 <umarfaruqu55@gmail.com>
2022-10-16 23:36:06 +0100

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 
 #include <stdio.h>
 /**
- * main - print  out size of data types in C
- * code by elsuraj66 
+ * main - print  out sizes of data types in C
+ * code by elsuraj66
  * Return: 0
 */
 int main(void)
