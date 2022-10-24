c6d22a4b6dddcd3a534631bd6f6c832cc088d31b
Elsuraj66 <umarfaruqu55@gmail.com>
2022-10-24 19:00:41 +0100

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,17 +4,19 @@
  *
  * Return: 0
  */
+
 int main(void)
 
 {
 	char letter;
 
 	for (letter 'a'; letter <= 'z'; letter++)
+	{
 
 	if (letter !='e' && letter !='q')
 
 	putchar(letter);
-	
+	}
 	putchar('\n');
 
 	return (0);
